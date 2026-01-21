f36888e01796bbc2ae9621586c3d3cd03217cc85
Shrinz <>
2026-01-21 18:20:43 -0500

    revised assign requirement

--- prototype1/script.js
+++ prototype1/script.js
@@ -8,7 +8,7 @@ const canvas = document.querySelector('.webgl')
 
 //Scene
 const scene = new THREE.Scene()
-scene.background = new THREE.Color('grey')
+scene.background = new THREE.Color('blanchedalmond')
 
 //Camera
 const camera = new THREE.PerspectiveCamera(
